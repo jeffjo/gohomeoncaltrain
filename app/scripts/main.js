a72dac50089c99459027ac6cb2cdb277cbc38800
Jeff Jo <jeffjo@squareup.com>
2013-11-20 00:32:28 -0800

    Use requirejs-text and requirejs-plugins
    -Fixture data can now be loaded using the requirejs json! plugin
    -Fixed outstanding JSHint issues
    -Converted MuniModel to use requirejs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -26,7 +26,9 @@ require.config({
         backbone: '../bower_components/backbone/backbone',
         underscore: '../bower_components/underscore/underscore',
         bootstrap: 'vendor/bootstrap',
-        handlebars: '../bower_components/handlebars/handlebars'
+        handlebars: '../bower_components/handlebars/handlebars',
+        text: '../bower_components/requirejs-text/text',
+        json: '../bower_components/requirejs-plugins/src/json'
     }
 });
 
--- app/scripts/models/muni_model.js
+++ app/scripts/models/muni_model.js
@@ -1,27 +1,19 @@
-GOHOMEONCALTRAIN.MuniRoute = (function(){
-    "use strict";
+/*global define*/
+
+define([
+],function() {
+    'use strict';
 
     function MuniRoute(attrs){
-      if ((typeof attrs !== "undefined") && (attrs !== null)){
-        this.id = attrs.id;
-        this.timeToDepartureStop = attrs.timeToDepartureStop;
-        this.departureStopId = attrs.departureStopId;
-        this.departureStopName = attrs.departureStopName;
-        this.arrivalStopId = attrs.arrivalStopId;
-      }
-
-      this.predictions = [];
-
+        if ((typeof attrs !== 'undefined') && (attrs !== null)){
+            this.id = attrs.id;
+            this.timeToDepartureStop = attrs.timeToDepartureStop;
+            this.departureStopId = attrs.departureStopId;
+            this.departureStopName = attrs.departureStopName;
+            this.arrivalStopId = attrs.arrivalStopId;
+        }
+        this.predictions = [];
     }
 
-
-    MuniRoute.loadFixture = function(){
-      //return an array of muni routes
-      GOHOMEONCALTRAIN.MuniRoutes = JSON.parse(GOHOMEONCALTRAIN.)
-    };
-
     return MuniRoute;
-}());
-
-
-
+});
