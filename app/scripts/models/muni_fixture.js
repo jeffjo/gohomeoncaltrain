f2eff31f3b6b992dcfcc95f65d219ddcfffc63d5
Forum Parmar and Jeff Jo <git+forump+jeffjo@squareup.com>
2013-11-19 17:25:19 -0800

    Add time to departure stop

--- app/scripts/models/muni_fixture.js
+++ app/scripts/models/muni_fixture.js
@@ -1,24 +1,28 @@
 [
   {
     "id": "47",
+    "timeToDepartureStop": 180,
     "departureStopId": "13243",
     "departureStopName": "11th & Mission",
     "arrivalStopId": "133163"
   },
   {
     "id": "83X",
+    "timeToDepartureStop": 240,
     "departureStopId": "17669",
     "departureStopName": "9th & Market",
     "arrivalStopId": "133163"
   },
   {
     "id": "N",
+    "timeToDepartureStop": 60,
     "departureStopId": "15419",
     "departureStopName": "Van Ness Station",
     "arrivalStopId": "15239"
   },
   {
     "id": "KT",
+    "timeToDepartureStop": 60,
     "departureStopId": "15419",
     "departureStopName": "Van Ness Station",
     "arrivalStopId": "17397"
