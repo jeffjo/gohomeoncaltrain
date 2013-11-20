18e26052fbc947beab766662f8e2dc281553801b
Forum Parmar and Jeff Jo <git+forump+jeffjo@squareup.com>
2013-11-19 17:20:35 -0800

    Add MUNI route fixture data

---
+++ app/scripts/models/muni_fixture.js
@@ -0,0 +1,26 @@
+[
+  {
+    "id": "47",
+    "departureStopId": "13243",
+    "departureStopName": "11th & Mission",
+    "arrivalStopId": "133163"
+  },
+  {
+    "id": "83X",
+    "departureStopId": "17669",
+    "departureStopName": "9th & Market",
+    "arrivalStopId": "133163"
+  },
+  {
+    "id": "N",
+    "departureStopId": "15419",
+    "departureStopName": "Van Ness Station",
+    "arrivalStopId": "15239"
+  },
+  {
+    "id": "KT",
+    "departureStopId": "15419",
+    "departureStopName": "Van Ness Station",
+    "arrivalStopId": "17397"
+  }
+]
