2f46900c87fa662b2608999adaa0726fc17597e3
Forum Parmar and Jeff Jo <git+forump+jeffjo@squareup.com>
2013-11-20 16:31:31 -0800

    Check muni predictions on a 30 second interval

--- app/scripts/views/muni_row.js
+++ app/scripts/views/muni_row.js
@@ -14,6 +14,9 @@ define([
         initialize: function() {
             this.model.bind("change", _.bind(this.render, this));
             this.model.fetch();
+            setInterval(function(){
+                this.model.fetch();
+            }.bind(this), 30000);
         },
         render: function() {
             this.$el.html(this.template(this.model.toJSON()));
