a64ed447696b2111042f4c55fbb676b5caa84553
Forum Parmar <forump@squareup.com>
2013-11-20 17:26:18 -0800

    Show muni departure and arrival times.

--- app/scripts/models/muni.js
+++ app/scripts/models/muni.js
@@ -1,9 +1,10 @@
 /*global define*/
 
 define([
+    'jquery',
     'underscore',
     'backbone'
-], function (_, Backbone) {
+], function ($, _, Backbone) {
     'use strict';
 
     var MuniModel = Backbone.Model.extend({
@@ -14,9 +15,23 @@ define([
         urlFormat: 'http://proximobus.appspot.com/agencies/sf-muni/stops/#{stopId}/predictions/by-route/#{routeId}.json',
 
         sync: function(method, model, options) {
-          var url = this.urlFormat.replace("#{stopId}", this.get('arrivalStopId'), "gi");
-          options.url = url.replace("#{routeId}", this.get('id'), "gi");
-          return Backbone.sync(method, model, options);
+          var url = this.urlFormat.replace("#{routeId}", this.get('id'), "gi");
+          var departureUrl = url.replace("#{stopId}", this.get('departureStopId'), "gi");
+          var arrivalUrl = url.replace("#{stopId}", this.get('arrivalStopId'), "gi");
+          options.url = departureUrl;
+          var self = this;
+          var params = {
+            type: "GET",
+            dataType: "json"
+          };
+          var departurePromise = Backbone.ajax(_.extend(params, options)).then(function(data) {
+            self.set('departurePredictions', data.items);
+          });
+          options.url = arrivalUrl;
+          var arrivalPromise = Backbone.ajax(_.extend(params, options)).then(function(data) {
+            self.set('arrivalPredictions', data.items);
+          });
+          return $.when(departurePromise, arrivalPromise);
         }
     });
 
